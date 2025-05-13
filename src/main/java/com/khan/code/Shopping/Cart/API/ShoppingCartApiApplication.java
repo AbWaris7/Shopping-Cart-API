8dfd3f412322110b79cf3789fa0d58fdbbc015a4
abdulwariskhan <hellokhan.ch@gmail.com>
2025-05-13 11:06:54 +0200

    - implemented Lombok

--- src/main/java/com/khan/code/Shopping/Cart/API/ShoppingCartApiApplication.java
+++ src/main/java/com/khan/code/Shopping/Cart/API/ShoppingCartApiApplication.java
@@ -1,5 +1,7 @@
 package com.khan.code.Shopping.Cart.API;
 
+import com.khan.code.Shopping.Cart.API.entities.Users;
+import org.apache.catalina.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +10,9 @@ public class ShoppingCartApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingCartApiApplication.class, args);
-		
+
+		var user = new Users(1L,"Abdul","abdul@gmail.com","122");
+
 	}
 
 }
--- src/main/java/com/khan/code/Shopping/Cart/API/entities/Users.java
+++ src/main/java/com/khan/code/Shopping/Cart/API/entities/Users.java
@@ -1,8 +1,16 @@
 package com.khan.code.Shopping.Cart.API.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "users")
 public class Users {
 
