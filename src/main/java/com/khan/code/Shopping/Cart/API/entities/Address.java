0632e9ed9bfe79eaae3979ed286ddfac9368d2ae
abdulwariskhan <hellokhan.ch@gmail.com>
2025-05-13 11:11:05 +0200

    - created Address Entity

---
+++ src/main/java/com/khan/code/Shopping/Cart/API/entities/Address.java
@@ -0,0 +1,35 @@
+package com.khan.code.Shopping.Cart.API.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "addresses")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "zip")
+    private String zip;
+
+    @Column(name = "state")
+    private String state;
+
+
+}
