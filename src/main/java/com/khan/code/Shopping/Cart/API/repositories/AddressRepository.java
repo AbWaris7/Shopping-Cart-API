217b495ae111968707a767ced24b30b5494538ed
abdulwariskhan <hellokhan.ch@gmail.com>
2025-05-15 08:54:29 +0200

    - added AddressRepository

---
+++ src/main/java/com/khan/code/Shopping/Cart/API/repositories/AddressRepository.java
@@ -0,0 +1,7 @@
+package com.khan.code.Shopping.Cart.API.repositories;
+
+import com.khan.code.Shopping.Cart.API.entities.Address;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AddressRepository extends CrudRepository<Address, Long> {
+}
